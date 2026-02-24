38c1e31e74e7b35c937835017e5ab608cc0a0567
SubbaReddy Jangalapalli <62408371+jangalapallisr@users.noreply.github.com>
2026-02-24 15:19:18 -0600

    Update snapmirror.sh

--- snapmirror.sh
+++ snapmirror.sh
@@ -1,4 +1,26 @@
-
+#!/bin/bash
+#set -x
+################################################################################################################################################################################################
+#Script Name:   SnapMirror_ONTAP2FSxN.sh
+#Author:        Subbu Jangalapalli
+#Date:          02/24/2026
+###Description:   This script is for to:
+#########################1. Create SnapMirror Relationship
+##########################2. Initialize SnapMirror Relationship
+###########################3. Update SnapMirror Relationship (Incremental Updates)
+############################4. Check SnapMirror Status
+#############################5. Schedule SnapMirror Updates
+#
+#Usage:     	<Script Name > <arg1> //arg1 is test or prod env;
+#Examle:    	./SnapMirror_ONTAP2FSxN.sh  <test>
+#
+# Change History:
+#----------------------------------------------------------------------------------------------------------------------------
+#Date            	  Name/ID              Change Description with reference Ticket number
+#02/24/2026        	Subbuj               Initial Creation of the script
+#
+##################################################################################################################################################################################################
+## ENV & Key Variables & Directories set up
 log_file="/tmp/my_snapmirror.sh"
 echo "snapmirror create -source-path svm_source:vol_source -destination-path svm_dest:vol_dest -type DP" >> $file_path
 echo "snapmirror initialize -destination-path svm_dest:vol_dest" >> $file_path
